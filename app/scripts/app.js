396e2010740cb46ce0af94aa46a9657fff2e02d2
Michaël Dayen <dayen.michael@gmail.com>
2016-06-22 17:23:51 -0400

    commit error
    test to delete this commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,6 +40,11 @@ angular
         // controller: 'AboutCtrl',
         // controllerAs: 'about'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
       .otherwise({
         redirectTo: '/'
       });
